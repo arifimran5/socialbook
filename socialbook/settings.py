2f734efb2fe56595991c6cf077ab90214cbfeb9c
arifimran5 <arifimran505@gmail.com>
2024-04-25 16:25:25 +0530

    add cors

--- socialbook/settings.py
+++ socialbook/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# CORS
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = ("http://localhost:4200",)
+CORS_ALLOW_HEADERS = ("*",)
+
 
 # Application definition
 
@@ -41,11 +46,13 @@ INSTALLED_APPS = [
     "posts",
     "rest_framework",
     "rest_framework.authtoken",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
